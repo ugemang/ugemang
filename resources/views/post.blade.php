668296f73a398a6816a6c0a15134d7d82315e5e4
elther <elther@naver.com>
2017-04-03 01:47:56 +0900

    Css change, add images

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -11,13 +11,62 @@
     <div class="title"><span class="name">타이탄폴2</span><span class="date">2017.01.01</span></div>
     <div class="subinfo"><span class="info">장르:fps</span><span class="info">출시일:2017.01.01</span></div>
     <div class="body">body</div>
-    <div class="files">첨부파일 : </div>
+    <div class="files"></div>
     <div class="previousArticle"><img src="/images/Arrow_left.png" alt="이전글"><span class="articleLink">이전글 : 리니지2</span></div>
     <div class="nextArticle"><img src="/images/Arrow_right.png" alt="다음글"><span class="articleLink">다음글 : 리니지 이터널</span></div>
 
   </div>
 
-  <div class="reply"></div>
+  <div class="board title2">
+    <span class="subject">타이탄폴2 댓글</span>
+    <span class="total">총 <label style="color:red">193개</label>의 댓글이 있습니다.</span>
+  </div>
 
+  <div class="replyArea">
+    <div class="reply">
+      <div class="userInfo"><img src="/images/img.png" class="userIcon"><span class="nick">돌고래야</span><span class="date">19:30:24</span><span class="modify">수정 / 삭제</span></div>
+      <div class="replyBody">댓글을 존나게 달아야합니다. 댓댓댓 댓글을 존나게 달아야합니다. 댓글을 존나게 달아야합니다.<br> 댓댓댓 댓글을 존나게 달아야합니다.
+      댓글을 존나게 달아야합니다. 댓댓댓 댓글을 존나게 달아야합니다. 댓글을 존나게 달아야합니다. 댓댓댓 댓글을 존나게 달아야합니다.<br>  댓글을 존나게 달아야합니다. 댓댓댓 댓글을 존나게 달아야합니다.
+          댓글을 존나게 달아야합니다. 댓댓댓 댓글을 존나게 달아야합니다. 댓글을 존나게 달아야합니다. 댓댓댓 댓글을 존나게 달아야합니다.<br>  댓글을 존나게 달아야합니다. 댓댓댓 댓글을 존나게 달아야합니다.
+        댓글을 존나게 달아야합니다. 댓댓댓 댓글을 존나게 달아야합니다. 댓글을 존나게 달아야합니다. 댓댓댓 댓글을 존나게 달아야합니다. 댓글을 존나게 달아야합니다. 댓댓댓 댓글을 존나게 달아야합니다.
+      댓글을 존나게 달아야합니다. 댓댓댓 댓글을 존나게 달아야합니다. 댓글을 존나게 달아야합니다. 댓댓댓 댓글을 존나게 달아야합니다. 댓글을<br>  존나게 달아야합니다. 댓댓댓 댓글을 존나게 달아야합니다.
+      댓글을 존나게 달아야합니다. 댓댓댓 댓글을 존나게 달아야합니다. 댓글을 존나게 달아야합니다. 댓댓댓 댓글을 존나게 달아야합니다. <br> 댓글을 존나게 달아야합니다. 댓댓댓 댓글을 존나게 달아야합니다.
+      댓글을 존나게 달아야합니다. 댓댓댓 댓글을 존나게 달아야합니다. 댓글을 존나게 달아야합니다.<br>  댓댓댓 댓글을 존나게 달아야합니다. 댓글을 존나게 달아야합니다. 댓댓댓 댓글을 존나게 달아야합니다.
+      </div>
+    </div>
+    <div class="reply">
+      <div class="userInfo"><img src="/images/img.png" class="userIcon"><span class="nick">돌고래야</span><span class="date">19:30:24</span><span class="modify">수정 / 삭제</span></div>
+      <div class="replyBody">댓글을 존나게 달아야합니다. 댓댓댓 댓글을 존나게 달아야합니다. 댓글을 존나게 달아야합니다.<br> 댓댓댓 댓글을 존나게 달아야합니다.
+      댓글을 존나게 달아야합니다. 댓댓댓 댓글을 존나게 달아야합니다. 댓글을 존나게 달아야합니다. 댓댓댓 댓글을 존나게 달아야합니다.<br>  댓글을 존나게 달아야합니다. 댓댓댓 댓글을 존나게 달아야합니다.
+          댓글을 존나게 달아야합니다. 댓댓댓 댓글을 존나게 달아야합니다. 댓글을 존나게 달아야합니다. 댓댓댓 댓글을 존나게 달아야합니다.<br>  댓글을 존나게 달아야합니다. 댓댓댓 댓글을 존나게 달아야합니다.
+        댓글을 존나게 달아야합니다. 댓댓댓 댓글을 존나게 달아야합니다. 댓글을 존나게 달아야합니다. 댓댓댓 댓글을 존나게 달아야합니다. 댓글을 존나게 달아야합니다. 댓댓댓 댓글을 존나게 달아야합니다.
+      댓글을 존나게 달아야합니다. 댓댓댓 댓글을 존나게 달아야합니다. 댓글을 존나게 달아야합니다. 댓댓댓 댓글을 존나게 달아야합니다. 댓글을<br>  존나게 달아야합니다. 댓댓댓 댓글을 존나게 달아야합니다.
+      댓글을 존나게 달아야합니다. 댓댓댓 댓글을 존나게 달아야합니다. 댓글을 존나게 달아야합니다. 댓댓댓 댓글을 존나게 달아야합니다. <br> 댓글을 존나게 달아야합니다. 댓댓댓 댓글을 존나게 달아야합니다.
+      댓글을 존나게 달아야합니다. 댓댓댓 댓글을 존나게 달아야합니다. 댓글을 존나게 달아야합니다.<br>  댓댓댓 댓글을 존나게 달아야합니다. 댓글을 존나게 달아야합니다. 댓댓댓 댓글을 존나게 달아야합니다.
+      </div>
+    </div>
+
+    <ul class="paging">
+      <li><<</li>
+      <li><</li>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+      <li>6</li>
+      <li>7</li>
+      <li>8</li>
+      <li>9</li>
+      <li>10</li>
+      <li>></li>
+      <li>>></li>
+    </ul>
+  </div>
+
+  <div class="comment">
+    <textarea class="writing"></textarea>
+    <div class="save"><p>제출</p></div>
+  </div>
 </div>
 @endsection
