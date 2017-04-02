44a2fde5b208ac222f54d21898ee99df53d4707b
elther <elther@naver.com>
2017-04-03 02:29:54 +0900

    Css change

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -47,11 +47,11 @@
     </div>
 
     <ul class="paging">
-      <li><<</li>
-      <li><</li>
+      <li class="sq"><<</li>
+      <li class="sq"><</li>
       <li>1</li>
       <li>2</li>
-      <li>3</li>
+      <li class="sq">3</li>
       <li>4</li>
       <li>5</li>
       <li>6</li>
@@ -59,8 +59,8 @@
       <li>8</li>
       <li>9</li>
       <li>10</li>
-      <li>></li>
-      <li>>></li>
+      <li class="sq">></li>
+      <li class="sq">>></li>
     </ul>
   </div>
 
