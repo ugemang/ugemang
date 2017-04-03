e64a02d259106b22662268a4fbceab77606b364d
Park <rolld@naver.com>
2017-04-03 18:19:23 +0900

    working on the main page

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,7 +1,26 @@
 @extends ('layouts.master')
 
 @section('content')
-  <div class="content">
-      maincontent
+  <div class="main-content">
+    <div class="main-first-row">
+      <div class="image-area">
+
+      </div>
+      <div class="login-area">
+
+      </div>
+    </div>
+    <div class="main-second-row">
+
+    </div>
+    <div class="main-third-row">
+
+    </div>
+    <div class="main-fourth-row">
+
+    </div>
+    <div class="main-fifth-row">
+
+    </div>
   </div>
 @endsection
