8f1df9284468be27a9e46d651361c8c84fc432ea
Park <rolld@naver.com>
2017-04-06 17:51:13 +0900

    headerdone

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -46,7 +46,7 @@
                     <img src="/images/news_ex.png" alt="">
                 </div>
                 <div class="tab-text">
-                    <div class="title">용감하고 굳세게</div>
+                    <div class="title">용감하고 굳세게 용감하고 굳세게</div>
                     <div class="summary">남는 것은 영락과 부패뿐이다! 낙원을 남는 것은 영락과 부패뿐이다! 낙원을</div>
                 </div>
             </div>
@@ -63,7 +63,7 @@
     </div>
     <div class="main-third-row">
         <div class="title-wrap">
-            <span class="news-title">Curation</span>
+            <span class="curation-title">Curation</span>
             <button class="view-more">
             </button>
         </div>
@@ -105,7 +105,7 @@
     <div class="main-fourth-row">
         <div class="new-post">
             <div class="title-wrap">
-                <span class="news-title">NEW POST</span>
+                <span class="new-title">NEW POST</span>
                 <button class="view-more">
                 </button>
             </div>
@@ -125,8 +125,6 @@
                             <div class="clicknum">119</div>
                         </div>
                     </li>
-                </ul>
-                <ul class="new-post-list">
                     <li>
                         <div class="cont-info">
                             <div class="no">5</div>
@@ -135,8 +133,6 @@
                             <div class="clicknum">119</div>
                         </div>
                     </li>
-                </ul>
-                <ul class="new-post-list">
                     <li>
                         <div class="cont-info">
                             <div class="no">5</div>
@@ -145,8 +141,6 @@
                             <div class="clicknum">119</div>
                         </div>
                     </li>
-                </ul>
-                <ul class="new-post-list">
                     <li>
                         <div class="cont-info">
                             <div class="no">5</div>
@@ -155,8 +149,6 @@
                             <div class="clicknum">119</div>
                         </div>
                     </li>
-                </ul>
-                <ul class="new-post-list">
                     <li>
                         <div class="cont-info">
                             <div class="no">5</div>
@@ -170,17 +162,177 @@
         </div>
         <div class="hot-post">
             <div class="title-wrap">
-                <span class="news-title">HOT POST</span>
+                <span class="hot-title">HOT POST</span>
                 <button class="view-more">
                 </button>
+            </div>
+            <div class="hot-post-wrap">
+                <div class="hot-post-top">
+                    <div class="sb01">NO</div>
+                    <div class="sb02">제목</div>
+                    <div class="sb03">등록일</div>
+                    <div class="sb04">조회수</div>
+                </div>
+                <ul class="hot-post-list">
+                    <li>
+                        <div class="cont-info">
+                            <div class="no">5</div>
+                            <div class="tit">용감하고 굳세게</div>
+                            <div class="regdate">2017-01-01</div>
+                            <div class="clicknum">119</div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="cont-info">
+                            <div class="no">5</div>
+                            <div class="tit">용감하고 굳세게</div>
+                            <div class="regdate">2017-01-01</div>
+                            <div class="clicknum">119</div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="cont-info">
+                            <div class="no">5</div>
+                            <div class="tit">용감하고 굳세게</div>
+                            <div class="regdate">2017-01-01</div>
+                            <div class="clicknum">119</div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="cont-info">
+                            <div class="no">5</div>
+                            <div class="tit">용감하고 굳세게</div>
+                            <div class="regdate">2017-01-01</div>
+                            <div class="clicknum">119</div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="cont-info">
+                            <div class="no">5</div>
+                            <div class="tit">용감하고 굳세게</div>
+                            <div class="regdate">2017-01-01</div>
+                            <div class="clicknum">119</div>
+                        </div>
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
     <div class="main-fifth-row">
         <div class="title-wrap">
-            <span class="news-title">SNS UPDATE</span>
+            <span class="sns-title">SNS UPDATE</span>
             <button class="view-more">
             </button>
+        </div>
+        <div class="sns-content-wrap">
+          <div class="sns-content">
+            <div class="sns-image">
+              <img src="/images/news_ex.png" alt="">
+              <div></div>
+            </div>
+            <div class="sns-info">
+              <div class="info-title">
+                용감하고 굳세게
+              </div>
+              <div class="info-summary">
+                <span>#온라인게임</span><span>#용감하고</span>굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게!굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게!
+              </div>
+              <div class="info-like">
+                <span class="like-txt">좋아요  58</span>
+                <span class="like-btn"></span>
+              </div>
+            </div>
+          </div>
+          <div class="sns-content">
+            <div class="sns-image">
+              <img src="/images/news_ex.png" alt="">
+              <div></div>
+            </div>
+            <div class="sns-info">
+              <div class="info-title">
+                용감하고 굳세게
+              </div>
+              <div class="info-summary">
+                <span>#온라인게임</span><span>#용감하고</span>굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게!굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게!
+              </div>
+              <div class="info-like">
+                <span class="like-txt">좋아요  58</span>
+                <span class="like-btn"></span>
+              </div>
+            </div>
+          </div>
+          <div class="sns-content">
+            <div class="sns-image">
+              <img src="/images/news_ex.png" alt="">
+              <div></div>
+            </div>
+            <div class="sns-info">
+              <div class="info-title">
+                용감하고 굳세게
+              </div>
+              <div class="info-summary">
+                <span>#온라인게임</span><span>#용감하고</span>굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게!굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게!
+              </div>
+              <div class="info-like">
+                <span class="like-txt">좋아요  58</span>
+                <span class="like-btn"></span>
+              </div>
+            </div>
+          </div>
+          <div class="sns-content">
+            <div class="sns-image">
+              <img src="/images/news_ex.png" alt="">
+              <div></div>
+            </div>
+            <div class="sns-info">
+              <div class="info-title">
+                용감하고 굳세게
+              </div>
+              <div class="info-summary">
+                <span>#온라인게임</span><span>#용감하고</span>굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게!굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게!
+              </div>
+              <div class="info-like">
+                <span class="like-txt">좋아요  58</span>
+                <span class="like-btn"></span>
+              </div>
+            </div>
+          </div>
+          <div class="sns-content">
+            <div class="sns-image">
+              <img src="/images/news_ex.png" alt="">
+              <div></div>
+            </div>
+            <div class="sns-info">
+              <div class="info-title">
+                용감하고 굳세게
+              </div>
+              <div class="info-summary">
+                <span>#온라인게임</span><span>#용감하고</span>굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게!굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게!
+              </div>
+              <div class="info-like">
+                <span class="like-txt">좋아요  58</span>
+                <span class="like-btn"></span>
+              </div>
+            </div>
+          </div>
+          <div class="sns-content">
+            <div class="sns-image">
+              <img src="/images/news_ex.png" alt="">
+              <div></div>
+            </div>
+            <div class="sns-info">
+              <div class="info-title">
+                용감하고 굳세게
+              </div>
+              <div class="info-summary">
+                <span>#온라인게임</span><span>#용감하고</span>굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게!굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게굳세게!
+              </div>
+              <div class="info-like">
+                <span class="like-txt">좋아요  58</span>
+                <span class="like-btn"></span>
+              </div>
+            </div>
+          </div>
         </div>
     </div>
   </div>
